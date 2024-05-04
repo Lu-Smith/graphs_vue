81a2c4a2ef5742523eed632bc260093166eb4fc8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-04 23:12:52 +0100

    Added MainGraphs.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import MainGraphs from './components/MainGraphs.vue';
+
 
 </script>
 
 <template>
   <div>
    <h1>Graphs</h1>
+   <MainGraphs />
   </div>
 </template>
 
