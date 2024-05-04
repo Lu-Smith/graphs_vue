309674b63cd25c1849b95ae20458240b6ca4c538
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-04 23:15:32 +0100

    Added MainFooter to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import MainGraphs from './components/MainGraphs.vue';
-
-
+import MainFooter from './components/MainFooter.vue';
 </script>
 
 <template>
   <div>
    <h1>Graphs</h1>
    <MainGraphs />
+   <MainFooter />
   </div>
 </template>
 
