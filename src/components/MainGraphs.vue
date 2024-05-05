efd12826b0b45ebff7df90bf057b079e335cb298
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-05 23:03:23 +0100

    Updated MainGraphs.vue

--- src/components/MainGraphs.vue
+++ src/components/MainGraphs.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const data = {
+        labels: ['January', 'February', 'March'],
+        datasets: [{ data: [40, 20, 12] }]
+}
+      
+
+</script>
+
 <template>
-    <h2>Main Page</h2>
+    <h2>Chart 1</h2>
+    <div class="chartContainer">
+        <Bar :data="data" />
+    </div>
 </template>
+
+<style scoped>
+.chartContainer {
+    background-color: antiquewhite;
+    padding: 20px;
+}
+</style>
