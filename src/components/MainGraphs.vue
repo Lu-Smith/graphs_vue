dd8198add9f940327f080bf52342dd9704a710bf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-06 13:49:22 +0100

    Updated MainGraph.vue

--- src/components/MainGraphs.vue
+++ src/components/MainGraphs.vue
@@ -13,7 +13,7 @@ import { Bar } from 'vue-chartjs'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-const chartData = ref({
+const birthsUKData = ref({
     labels: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s'],
     datasets: [
     {
@@ -31,18 +31,84 @@ const chartData = ref({
   ]
 })
 
+const birthsWorldData = ref({
+    labels: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s'],
+    datasets: [
+    {
+    label: 'Number of Births Worldwide in mln',
+      data: [91.3, 103.7, 109.3, 116.4, 120.9, 133.4], 
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)', 
+        'rgba(54, 162, 235, 0.5)', 
+        'rgba(255, 206, 86, 0.5)', 
+        'rgba(75, 192, 192, 0.5)', 
+        'rgba(153, 102, 255, 0.5)', 
+        'rgba(255, 159, 64, 0.5)' 
+      ]
+    }
+  ]
+})
+
+const deathsUKData = ref({
+  labels: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s'],
+  datasets: [
+    {
+      label: 'Number of Deaths in UK in mln',
+      data: [0.7, 0.8, 0.9, 1.0, 1.1, 1.2], 
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)', 
+        'rgba(54, 162, 235, 0.5)', 
+        'rgba(255, 206, 86, 0.5)', 
+        'rgba(75, 192, 192, 0.5)', 
+        'rgba(153, 102, 255, 0.5)', 
+        'rgba(255, 159, 64, 0.5)' 
+      ]
+    }
+  ]
+})
+
+const deathsWorldData = ref({
+  labels: ['1960s', '1970s', '1980s', '1990s', '2000s', '2010s'],
+  datasets: [
+    {
+      label: 'Number of Deaths Worldwide in mln',
+      data: [33.9, 36.5, 39.5, 43.5, 48.1, 55.3], 
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)', 
+        'rgba(54, 162, 235, 0.5)', 
+        'rgba(255, 206, 86, 0.5)', 
+        'rgba(75, 192, 192, 0.5)', 
+        'rgba(153, 102, 255, 0.5)', 
+        'rgba(255, 159, 64, 0.5)' 
+      ]
+    }
+  ]
+})
+
 const chartOptions = ref({
   responsive: true
-})
-      
+})   
+
+const title = ref(0);
+const titles = ['Births in UK', 'Births in the world','Deaths in UK', 'Deaths in the world'];
+const data = [birthsUKData, birthsWorldData, deathsUKData, deathsWorldData];
+
+const handleGraph = () => {
+   if (title.value < titles.length - 1) title.value++;
+   else title.value = 0;
+}
 </script>
 
 <template>
-    <h2>Chart 1</h2>
+    <div>
+        <h2>{{ titles[title] }}</h2>
+        <button @click="handleGraph" >Next</button>
+    </div>
+
     <div class="chartContainer">
         <Bar
             :options="chartOptions"
-            :data="chartData"
+            :data="data[title].value"
         />
     </div>
 </template>
