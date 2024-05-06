723326c817c231fc3c54e20218b9f29da0585d31
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-06 14:10:11 +0100

    Updated MainFooter.vue

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <h4>
+    <h4 class="footer">
         Coded by 
         <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-source 
         <a href="https://github.com/Lu-Smith/graphs_vue" target="_blank" >GitHub</a>.
